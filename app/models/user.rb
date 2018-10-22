467e8d27a29b14444dc7b1f33457d9f2f2bf8201
hoanghung123vn <20151845@student.hust.edu.vn>
2018-10-22 22:41:25 +0700

    Log in, Log out part 1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token, :activation_token, :reset_token
+  before_save :downcase_email
+  before_create :create_activation_digest
   has_many :user_curriculum_vitaes
   has_many :curriculum_vitaes, through: :user_curriculum_vitaes
   has_many :user_jobs
@@ -14,4 +17,66 @@ class User < ApplicationRecord
   validates :phone, presence: true, numericality: true, length: {minimum: Settings.min_length_phone, maximum: Settings.max_length_phone}
   validates :address, presence: true, length: {maximum: Settings.max_length_add}
   validates :role, presence: true, numericality: true, length: {maximum: Settings.role}
+  class << self
+    def digest string
+      cost = if ActiveModel::SecurePassword.min_cost
+               BCrypt::Engine::MIN_COST
+             else
+               BCrypt::Engine.cost
+             end
+      BCrypt::Password.create(string, cost: cost)
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update remember_digest: User.digest(remember_token)
+  end
+
+  def authenticated? attribute, token
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+  end
+
+  def forget
+    update remember_digest: nil
+  end
+
+  def activate
+    update activated: true, activated_at: Time.zone.now
+  end
+
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update reset_digest: User.digest(reset_token)
+    update reset_sent_at: Time.zone.now
+  end
+
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  def password_reset_expired?
+    reset_sent_at < Settings.expire_time.hours.ago
+  end
+
+  private
+
+  def downcase_email
+    email.downcase!
+  end
+
+  def create_activation_digest
+    self.activation_token  = User.new_token
+    self.activation_digest = User.digest(activation_token)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
+    default_url_options :host => "localhost:3000"
     root "static_pages#home"
     get "/signup", to: "users#new"
     post "/signup", to: "users#create"
+    get "/login", to: "sessions#new"
+    post "/login", to: "sessions#create"
+    delete "/logout", to: "sessions#destroy"
+    get "/password_resets/new", to: "password_resets#new"
+    get "password_resets/edit", to: "password_resets#edit"
     resources :users
+    resources :account_activations, only: [:edit]
+    resources :password_resets, only: [:new, :create, :edit, :update]
   end
 end
